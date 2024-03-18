6059468366d96549272426f82ab5950439d1d0c1
Tomas Sychra <sychrat@gmail.com>
2024-03-18 15:03:29 +0100

    api, routing

--- api/provider/RequestProvider.php
+++ api/provider/RequestProvider.php
@@ -3,15 +3,34 @@
 class RequestProvider
 {
 
+  private function get_data($attributes, $params): array {
+    return [
+      'data' => 'data'
+    ];
+  }
+
   public function create_response (): array {
     $method = $_SERVER['REQUEST_METHOD'];
+    $queryString = $_SERVER['QUERY_STRING'];
 
+    // Parse URL and get url attributes
+    $parsedUrl = $_GET['url'] ?? '';
+    $attributes = array_filter(explode('/', $parsedUrl));
+
+    // Parse QUERY and get url params
+    $params = [];
+    parse_str($queryString, $params);
+
+    $data = $this -> get_data($attributes, $params);
 
     return [
-      'data' => 'Some data here',
+      'data' => $data,
       // TODO
       'method' => $method,
-      'test_x' => PROJECT['name']
+      'test_x' => PROJECT['name'],
+      // TODO
+      'attributes' => $attributes,
+      'params' => $params,
     ];
   }
 
