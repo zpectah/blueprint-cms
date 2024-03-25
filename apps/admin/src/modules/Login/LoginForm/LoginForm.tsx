9210168897432c3f8baae34e66c874616ca7c94f
Tomas Sychra <sychrat@gmail.com>
2024-03-25 18:53:07 +0100

    apps admin - login form ui

--- apps/admin/src/modules/Login/LoginForm/LoginForm.tsx
+++ apps/admin/src/modules/Login/LoginForm/LoginForm.tsx
@@ -25,72 +25,74 @@ const LoginForm = () => {
   };
 
   return (
-    <Card variant="elevation" elevation={1}>
-      <CardContent sx={{}}>
-        <FormBlock
-          boxProps={{
-            sx: {
-              paddingTop: `calc(${SPACING_BASE} * 2)`,
-              paddingBottom: `calc(${SPACING_BASE} * 2)`,
-              textAlign: 'center',
-            },
-          }}
-        >
-          <Typography variant="h1" sx={{ lineHeight: 1 }}>
-            {CFG_CMS.name}
-          </Typography>
-        </FormBlock>
-      </CardContent>
-      <Divider />
-      <CardContent>
-        <Form onSubmit={handleSubmit(submitHandler)}>
-          <FormBlock>
-            <FormField fieldId="login_email">
-              <Controller
-                control={control}
-                name="email"
-                rules={{ required: true }}
-                render={({ field, fieldState }) => (
-                  <EmailInput
-                    id="login_email"
-                    placeholder="Type your email"
-                    size="medium"
-                    error={!!fieldState.error}
-                    fullWidth
-                    {...field}
-                  />
-                )}
-              />
-            </FormField>
-            <FormField fieldId="login_password">
-              <Controller
-                control={control}
-                name="password"
-                rules={{ required: true }}
-                render={({ field, fieldState }) => (
-                  <PasswordInput
-                    id="login_password"
-                    placeholder="Type your password"
-                    size="medium"
-                    error={!!fieldState.error}
-                    fullWidth
-                    {...field}
-                  />
-                )}
-              />
-            </FormField>
+    <>
+      <FormBlock
+        boxProps={{
+          sx: {
+            paddingTop: SPACING_BASE,
+            paddingBottom: `calc(${SPACING_BASE} * 2)`,
+            textAlign: 'center',
+          },
+        }}
+      >
+        <Typography variant="h1" sx={{ lineHeight: 1 }}>
+          {CFG_CMS.name}
+        </Typography>
+      </FormBlock>
+      <Card variant="elevation" elevation={1}>
+        <CardContent sx={{ textAlign: 'center' }}>
+          <FormBlock boxProps={{ sx: { py: 3 } }}>
+            <Typography variant="h4">Login to system</Typography>
           </FormBlock>
-          <FormFooter direction="column" justify="center" dense={false}>
-            <Button type="submit" variant="contained" size="large" disabled={!isValid} fullWidth>
-              Login
-            </Button>
-            <Button variant="text" size="large" color="secondary" fullWidth>
-              Lost password
-            </Button>
-          </FormFooter>
-        </Form>
-      </CardContent>
-    </Card>
+          <Form onSubmit={handleSubmit(submitHandler)}>
+            <FormBlock>
+              <FormField fieldId="login_email">
+                <Controller
+                  control={control}
+                  name="email"
+                  rules={{ required: true }}
+                  render={({ field, fieldState }) => (
+                    <EmailInput
+                      id="login_email"
+                      placeholder="Type your email"
+                      size="medium"
+                      error={!!fieldState.error}
+                      fullWidth
+                      {...field}
+                    />
+                  )}
+                />
+              </FormField>
+              <FormField fieldId="login_password">
+                <Controller
+                  control={control}
+                  name="password"
+                  rules={{ required: true }}
+                  render={({ field, fieldState }) => (
+                    <PasswordInput
+                      id="login_password"
+                      placeholder="Type your password"
+                      size="medium"
+                      error={!!fieldState.error}
+                      fullWidth
+                      {...field}
+                    />
+                  )}
+                />
+              </FormField>
+            </FormBlock>
+            <FormFooter direction="column" justify="center" dense={false}>
+              <Button type="submit" variant="contained" size="large" disabled={!isValid} fullWidth>
+                Login
+              </Button>
+              <Button variant="text" size="large" color="secondary" fullWidth>
+                Lost password
+              </Button>
+            </FormFooter>
+          </Form>
+        </CardContent>
+      </Card>
+    </>
   );
 };
 
